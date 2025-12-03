dda5cb29f775130757470ffaf12f7a505314cf1e
vtg <vtg@qad.com>
2025-12-03 10:16:48 +0200

    Initial commit

---
+++ src/main/java/com/extensions/bcbrowseapp/MyExt.java
@@ -0,0 +1,19 @@
+package com.extensions.bcbrowseapp;
+
+import com.qad.ipc.dto.BCExecutionError;
+import com.qad.ipc.dto.Output;
+import com.qad.ipc.service.Extension;
+import com.qad.sales.salesorder.SalesOrderHeaderBaseService;
+import com.qad.sales.salesorder.SalesOrderHeaderDataSet;
+
+@Extension
+public class MyExt extends SalesOrderHeaderBaseService {
+
+    @Override
+    public void fetch(String domainCode, String salesOrderNumber, Output<SalesOrderHeaderDataSet> dsSalesOrderHeader)
+            throws BCExecutionError {
+        // TODO Auto-generated method stub
+        super.fetch(domainCode, salesOrderNumber, dsSalesOrderHeader);
+    }
+
+}
